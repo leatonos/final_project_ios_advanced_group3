93b461d7e436f65ac63713fc68579526ad635339
Pedro Fernando <pedrofmattosb@gmail.com>
2021-05-18 01:10:40 -0400

    First upload
    2 Screens and table view ready

---
+++ Final_project/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Final_project
+//
+//  Created by user198868 on 5/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let categories = [
+        "Secrets",
+        "Events",
+        "Meetings"
+    
+    ]
+    
+
+    @IBOutlet var categoryTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        categoryTableView.delegate = self
+        categoryTableView.dataSource = self
+        
+    }
+
+}
+
+extension ViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me")
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        
+        
+        return cell
+    }
+    
+}
+
